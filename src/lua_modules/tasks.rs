b21d071568d8a60564ed1d3bbb3c6d457d22d7dc
Tom Clark <the.thomas.j.clark@gmail.com>
2025-11-01 15:17:03 -0400

    WIP: coroutines

--- src/lua_modules/tasks.rs
+++ src/lua_modules/tasks.rs
@@ -4,14 +4,22 @@ use eldenring::{
 };
 use eldenring_util::task::CSTaskImpExt;
 use fromsoftware_shared::get_instance;
-use mlua::prelude::*;
+use mlua::{chunk, prelude::*};
 use std::sync::{Arc, Mutex};
+
+const TASK_END: i32 = 0;
+const TASK_RESTART: i32 = 1;
+
+struct Task {
+    func: LuaFunction,
+    thread: LuaThread,
+}
 
 /**
  * Lua API for registering recurring tasks
  */
 pub(crate) struct Tasks {
-    tasks: Arc<Mutex<Vec<LuaFunction>>>,
+    tasks: Arc<Mutex<Vec<Task>>>,
 }
 
 impl Tasks {
@@ -20,19 +28,39 @@ impl Tasks {
             tasks: Arc::new(Mutex::new(Vec::new())),
         };
 
+        let tasks = Arc::downgrade(&result.tasks);
+
         let cs_task = unsafe { get_instance::<CSTaskImp>() }.unwrap();
         cs_task.run_recurring(
-            {
-                let tasks = Arc::downgrade(&result.tasks);
-                move |data: &FD4TaskData| {
-                    if let Some(tasks) = tasks.upgrade() {
-                        for task in tasks.lock().unwrap().iter() {
-                            if let Err(e) = task.call::<()>(data.delta_time.time) {
+            move |_: &FD4TaskData| {
+                let Some(tasks) = tasks.upgrade() else { return };
+                tasks
+                    .lock()
+                    .unwrap()
+                    .retain(|task| match task.thread.resume(()) {
+                        Ok(Some(TASK_END)) => {
+                            println!("EndEvent");
+                            false
+                        }
+                        Ok(Some(TASK_RESTART)) => {
+                            println!("RestartEvent");
+                            if let Err(e) = task.thread.reset(task.func.clone()) {
                                 println!("{}", e);
+                                false
+                            } else {
+                                true
                             }
                         }
-                    }
-                }
+                        Ok(None) => {
+                            println!("continue");
+                            true
+                        }
+                        Err(e) => {
+                            println!("{}", e);
+                            false
+                        }
+                        _ => true,
+                    });
             },
             CSTaskGroupIndex::FrameBegin,
         );
@@ -41,19 +69,39 @@ impl Tasks {
     }
 
     pub fn register(&self, lua: &Lua) -> LuaResult<()> {
-        lua.globals().set(
-            "InitializeTask",
-            lua.create_function({
-                let tasks = Arc::downgrade(&self.tasks);
-                move |_, func: LuaFunction| {
-                    if let Some(tasks) = tasks.upgrade() {
-                        tasks.lock().unwrap().push(func);
-                    }
-
-                    Ok(())
+        let initialize_task = lua.create_function({
+            let tasks = Arc::downgrade(&self.tasks);
+            move |lua, func: LuaFunction| {
+                if let Some(tasks) = tasks.upgrade() {
+                    tasks.lock().unwrap().push(Task {
+                        func: func.clone(),
+                        thread: lua.create_thread(func)?,
+                    });
                 }
-            })?,
-        )
+                Ok(())
+            }
+        })?;
+
+        let end_task = lua
+            .load(chunk! {
+                function()
+                    coroutine.yield($TASK_END)
+                end
+            })
+            .eval::<LuaFunction>()?;
+
+        let restart_task = lua
+            .load(chunk! {
+                function()
+                    coroutine.yield($TASK_RESTART)
+                end
+            })
+            .eval::<LuaFunction>()?;
+
+        let globals = lua.globals();
+        globals.set("InitializeEvent", initialize_task)?;
+        globals.set("EndEvent", end_task)?;
+        globals.set("RestartEvent", restart_task)
     }
 
     /**
@@ -62,7 +110,7 @@ impl Tasks {
      */
     pub fn drop_environment(&self, environment: &LuaTable) {
         self.tasks.lock().unwrap().retain(|task| {
-            if let Some(task_env) = task.environment() {
+            if let Some(task_env) = task.func.environment() {
                 task_env != *environment
             } else {
                 true
